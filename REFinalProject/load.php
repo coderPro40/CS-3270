56b97008c61a562ee3e6a631989f2e9ca42760f2
OfurumTG <thankgodcofurum@gmail.com>
2017-05-20 17:53:19 +0000

    save, load

--- REFinalProject/load.php
+++ REFinalProject/load.php
@@ -2,16 +2,16 @@
 require_once 'page.php';
 
 // create connection to mysql database
-$conn = mysql_connect("localhost", "tofurum", "si1170zv");
-mysql_select_db("tofurum");
-$res = mysql_query("select * from SavedGame");
+$conn = mysqli_connect("127.0.0.1", "coderproto", "");
+mysqli_select_db($conn, "tofurum");
+$res = mysqli_query($conn, "select * from SavedGame");
 
-$column = mysql_fetch_field($res);                  // fetch columns in asset DB
+$column = mysqli_fetch_field($res);                 // fetch columns in asset DB
 $_SESSION['brdObject2'] = $column[0];
 $choice = $column[1];
 $_SESSION['config2'] = ($choice == 'yes')? 1: 0;
 
-mysql_close($conn);                                 // close connection to db
+mysqli_close($conn);                                // close connection to db
 header('Location:game.php');                        // return to game
 exit;
 ?>
